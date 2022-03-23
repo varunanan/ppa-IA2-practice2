0e25322f6dd68ecb1f37b46f0d5b27108af99f61
Varun <94883501+varunanan@users.noreply.github.com>
2022-03-23 09:06:57 +0000

    commited

---
+++ p3original.c
@@ -0,0 +1,44 @@
+#include<stdio.h>
+int input_number()
+{
+  int n;
+  printf("Enter a number: \n");
+  scanf("%d",&n);
+  return n;
+}
+int is_composite(int n)
+{
+   int i;
+  
+  if(n==0|| n==1)
+    return 2;
+  else
+  {
+  for (i = 2; i <= n / 2; i++)
+  {
+    if (n % i == 0) 
+    {
+      return 1;
+    }
+  }
+    return 0;
+  }
+}
+  
+void output(int n,int composite)
+{ 
+  if(composite==0)
+  printf("The number %d is prime number",n);
+  else if(composite==1)
+  printf("The number %d is composite number",n);
+  else
+  printf("The number 0 & 1 is neither prime nor composite number");
+}
+int main()
+{
+  int n,composite;
+  n=input_number();
+  composite=is_composite(n);
+  output(n,composite);
+  return 0;
+}
